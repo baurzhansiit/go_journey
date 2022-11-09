46c2c8abde985fdd7a921bfcb2c9bd0c455a2898
Baurzhansiit <baurzhansiit@gmail.com>
2022-11-09 09:20:27 -0600

    t1

--- main.go
+++ main.go
@@ -17,20 +17,29 @@ func main() {
 		fmt.Println("[ INFO ] Add string argument eg. <g 'comment'> before Pushing to Github")
 
 	} else {
-		_, err := exec.Command("git", "log").Output()
-		ErrHandler(err)
-		fmt.Printf("check was passed: ")
 		fmt.Printf("1. Github pushing with commits:  %s\n", os.Args[1])
 		out, err := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD").Output()
-		ErrHandler(err)
+		if err != nil {
+			fmt.Println("There no git repository available")
+			panic(err.Error())
+		}
 		fmt.Println(string(out[:len(out)-1]))
 		_, err = exec.Command("git", "add", "./").Output()
-		ErrHandler(err)
+		if err != nil {
+			fmt.Println("err when add")
+			panic(err)
+		}
 		_, err = exec.Command("git", "commit", "-am", os.Args[1]).Output()
-		ErrHandler(err)
+		if err != nil {
+			fmt.Println("err when commit")
+			panic(err)
+		}
 		branchName := string(out[:len(out)-1])
 		_, err = exec.Command("git", "push", "origin", branchName).Output()
-		ErrHandler(err)
+		if err != nil {
+			fmt.Println("err when push")
+			panic(err)
+		}
 		fmt.Printf("2. Pushed commit to branch name: %s\n", out)
 	}
 
